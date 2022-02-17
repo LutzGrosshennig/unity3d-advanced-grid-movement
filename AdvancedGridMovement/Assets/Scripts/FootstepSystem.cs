fe6c02b361429e7641901c0253a52a7b487e5bd9
Lutz Großhennig <lutz@lutzgrosshennig.de>
2022-02-17 17:26:11 +0100

    Added pitch settings

--- AdvancedGridMovement/Assets/Scripts/FootstepSystem.cs
+++ AdvancedGridMovement/Assets/Scripts/FootstepSystem.cs
@@ -2,35 +2,50 @@ using UnityEngine;
 
 public class FootstepSystem : MonoBehaviour
 {
+    [Header("Left foot settings")]
     [SerializeField] private AudioSource leftFoot;
     [SerializeField] private AudioClip leftFootClip;
+    [Range(0.1f, 1.0f)]
+    [SerializeField] private float leftMinimumPitch = 0.5f;
 
+    [Header("Right foot settings")]
     [SerializeField] private AudioSource rightFoot;
     [SerializeField] private AudioClip rightFootClip;
+    [Range(0.1f, 1.0f)]
+    [SerializeField] private float rightMinimumPitch = 0.5f;
 
+    [Header("turn around settings")]
     [SerializeField] private AudioClip turnClip;
+    [Range(0.1f, 1.0f)]
+    [SerializeField] private float turnMinimumPitch = 0.5f;
+
+
 
     private bool nextStepLeft = false;
 
+
+
     public void Step()
     {
-        if(nextStepLeft)
+        if (nextStepLeft)
         {
-                leftFoot.PlayOneShot(leftFootClip, Random.Range(0.2f, 0.5f));
-                nextStepLeft = !nextStepLeft;
+            leftFoot.pitch = Random.Range(leftMinimumPitch, 1f);
+            leftFoot.PlayOneShot(leftFootClip);
+            nextStepLeft = !nextStepLeft;
         }
         else
         {
-                rightFoot.PlayOneShot(rightFootClip, Random.Range(0.2f, 0.5f));
-                nextStepLeft = !nextStepLeft;
+            rightFoot.pitch = Random.Range(rightMinimumPitch, 1f);
+            rightFoot.PlayOneShot(rightFootClip);
+            nextStepLeft = !nextStepLeft;
         }
     }
 
     public void Turn()
     {
-        leftFoot.pitch = Random.Range(0.7f, 1f);
-        rightFoot.pitch = Random.Range(0.7f, 1f);
-        leftFoot.PlayOneShot(turnClip, Random.Range(0.1f, 0.3f));
-        rightFoot.PlayOneShot(turnClip, Random.Range(0.1f, 0.3f));
+        leftFoot.pitch = Random.Range(turnMinimumPitch, 1f);
+        rightFoot.pitch = Random.Range(turnMinimumPitch, 1f);
+        leftFoot.PlayOneShot(turnClip);
+        rightFoot.PlayOneShot(turnClip);
     }
 }
